32bae621b815bc2638c39229f9240c26dd1dae3b
Yanhao Zhu <yanhaozhu@gmail.com>
2017-05-07 17:34:27 -0700

    Initial commit with roadrunner docs

---
+++ rust/doc/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["roadrunner"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;ParseError&gt; for <a class=\"enum\" href=\"roadrunner/enum.Error.html\" title=\"enum roadrunner::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://hyperium.github.io/hyper/hyper/uri/struct.UriError.html\" title=\"struct hyper::uri::UriError\">UriError</a>&gt; for <a class=\"enum\" href=\"roadrunner/enum.Error.html\" title=\"enum roadrunner::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"https://hyperium.github.io/hyper/hyper/error/enum.Error.html\" title=\"enum hyper::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"roadrunner/enum.Error.html\" title=\"enum roadrunner::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html\" title=\"struct std::io::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"roadrunner/enum.Error.html\" title=\"enum roadrunner::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/serde_json/1.0.1/serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"roadrunner/enum.Error.html\" title=\"enum roadrunner::Error\">Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rust/doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["roadrunner"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"roadrunner/struct.Content.html\" title=\"struct roadrunner::Content\">Content</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"roadrunner/struct.Response.html\" title=\"struct roadrunner::Response\">Response</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"roadrunner/enum.Error.html\" title=\"enum roadrunner::Error\">Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
